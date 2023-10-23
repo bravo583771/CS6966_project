e5c2d2c5dc1a754b1dd28b84844ac3e035e38d3e
Cen-Jhih Li <45063223+bravo583771@users.noreply.github.com>
2023-10-23 12:07:09 -0600

    fix some bugs for the basic framework
    This framework is successfully run on CHPC.
    Using new prompts from Jiaxuan-Peng

--- prompt.py
+++ prompt.py
@@ -4,6 +4,25 @@ import torch
 import jsonlines 
 import random 
 import os
+from sklearn.metrics import f1_score
+
+def iou_f1_score(generated_explanation, target_explanation):
+    # Convert explanations to sets of words
+    generated_set = set(generated_explanation.split())
+    target_set = set(target_explanation.split())
+
+    # Calculate the intersection and union of the sets
+    intersection = len(generated_set.intersection(target_set))
+    union = len(generated_set) + len(target_set) - intersection
+
+    # Calculate IOU F1 score
+    if union == 0:
+        return 1.0  # Handle the case of an empty union
+    else:
+        iou = intersection / union
+        f1 = 2 * (iou) / (iou + 1)  # Calculate F1 score from IOU
+        return f1
+    
 
 def newyorker_caption_contest_data(args):
     from datasets import load_dataset
@@ -29,9 +48,9 @@ def newyorker_caption_contest_data(args):
 
 def newyorker_caption_contest_llama2(args): 
     print("Loading data")
-    nyc_data = newyorker_caption_contest_data(args)
-    nyc_data_five_val = random.sample(nyc_data['test'],5)
-    nyc_data_train_two = random.sample(nyc_data['train'],2)
+    #nyc_data = newyorker_caption_contest_data(args)
+    #nyc_data_five_val = random.sample(nyc_data['test'],5)
+    #nyc_data_train_two = random.sample(nyc_data['train'],2)
     
     print ("Loading data")
     nyc_data_five_val = []
@@ -71,25 +90,19 @@ def newyorker_caption_contest_llama2(args):
     pipeline = transformers.pipeline(
         "text-generation",
         model=args.llama2_checkpoint,
-        cache_dir = args.cache_dir,
+        #cache_dir = args.cache_dir,
         torch_dtype=torch.float16,
         device_map="auto",
     )
 
     for i, val_inst in enumerate(nyc_data_five_val):         
         # ======================> ADD YOUR CODE TO DEFINE A PROMPT WITH TWO TRAIN EXAMPLES/DEMONSTRATIONS/SHOTS <======================
-        prompt = "<s>[INST] <<SYS>>\n" +\
-            "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  \
-                Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. \
-                    Please ensure that your responses are socially unbiased.\n" +\
-            "If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. \
-                If you don't know the answer to a question, please don't share false information.\n" + "<</SYS>>\n" +\
-            "Can you answer whether the following movie review is positive or not and then report phrases in the review that are important for deciding on your answer?" +\
-                  "The movie review is " + nyc_data_train_two[0]['input'] + "[/INST]" + nyc_data_train_two[0]['target'] + "</s><s>[INST]" +\
-                         "Can you answer whether the following movie review is positive or not and then report phrases in the review that are important for deciding on your answer?" +\
-                             "The movie review is " + nyc_data_train_two[1]['input'] + "[/INST]" + nyc_data_train_two[1]['target'] +\
-                             "</s><s>[INST]" + "Can you answer whether the following movie review is positive or not and then report phrases in the review that are important for deciding on your answer?" + \
-                                "The description is " + val_inst['input'] + "[/INST]"
+        prompt =  "Please use yes or not to answer whether the movie review is positive or not and then use report important phrases to explain the reason?" + \
+            nyc_data_train_two[0]['input'] + "[/INST]" + nyc_data_train_two[0]['target'] + "</s><s>[INST]" + \
+                "Please use yes or not to answer whether the movie review is positive or not and then use report important phrases to explain the reason?" +\
+                      nyc_data_train_two[1]['input'] + "[/INST]"  + nyc_data_train_two[1]['target'] + "</s><s>[INST]" + \
+                        "According to the above two examples, please use yes or not to answer whether the movie review is positive or not and then use report important phrases to explain the reason?" + \
+                              val_inst['input'] + "[/INST]"         
 
         sequences = pipeline(
             prompt,
@@ -100,6 +113,11 @@ def newyorker_caption_contest_llama2(args):
         
         gen_expl = sequences[0]['generated_text'].split("/INST]")[-1]
         nyc_data_five_val[i]['generated_llama2']=gen_expl
+
+        #calculate Intersection-over-Union (IOU) F1
+        target_explanation = nyc_data_five_val[i]['target']
+        iou_f1 = iou_f1_score(gen_expl, target_explanation)
+        nyc_data_five_val[i]['iou_f1_score'] = iou_f1
 
 
     filename = 'out/val.jsonl'
@@ -119,7 +137,7 @@ if __name__ == '__main__':
     parser.add_argument('--output_dir', type=str, help='Directory where model checkpoints will be saved')
     parser.add_argument('--cache_dir', type=str, help='Directory where cache will be saved')
     parser.add_argument('--task_name', default="imdb",  type=str, help='Name of the task that will be used by huggingface load dataset')    
-    parser.add_argument('--subtask', default="explanation", type=str, help="The contest has three subtasks: matching, ranking, explanation")
+    #parser.add_argument('--subtask', default="explanation", type=str, help="The contest has three subtasks: matching, ranking, explanation")
     parser.add_argument('--llama2_checkpoint', default="meta-llama/Llama-2-7b-chat-hf", type=str, help="The hf name of a llama2 checkpoint")
     args = parser.parse_args()
 
