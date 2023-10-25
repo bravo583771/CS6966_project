f9a968f82c9a1df279378c9c4d9b9c5829224067
Cen-Jhih Li <45063223+bravo583771@users.noreply.github.com>
2023-10-24 19:24:06 -0600

    a new version for movie_rationales
    update prompt
    update data loading function
    update the input for iou computing
    change max_length for pipeline from 1024 to 4096
    
    In this version, the model can generate a reasonable hightlight and prediction.
    However, when the review's label is negative, the model will answer "yes the review is negative" while the correct answer should be "no, the review is negative(not positive)"

--- prompt.py
+++ prompt.py
@@ -24,23 +24,23 @@ def iou_f1_score(generated_explanation, target_explanation):
         return f1
     
 
-def newyorker_caption_contest_data(args):
+def newyorker_caption_contest_data(task_name = 'movie_rationales'):
     from datasets import load_dataset
-    dset = load_dataset(args.task_name, cache_dir = args.cache_dir)
+    dset = load_dataset(task_name)
 
     res = {}
-    for spl, spl_name in zip([dset['train'], dset['test']],
-                            ['train', 'test']):
+    for spl, spl_name in zip([dset['train'], dset['validation'], dset['test']],
+                            ['train', 'val', 'test']):
         cur_spl = []
         for inst in list(spl):
-            inp = inst['text']
+            inp = inst['review']
             targ = inst['label']
-            cur_spl.append({'input': inp, 'target': targ})
+            highlight = inst['evidences']
+            cur_spl.append({'input': inp, 'target': targ, 'highlight': highlight})
         
-            #'input' is an image annotation we will use for a llama2 e.g. "scene: the living room description: A man and a woman are sitting on a couch. They are surrounded by numerous monkeys. uncanny: Monkeys are found in jungles or zoos, not in houses. entities: Monkey, Amazon_rainforest, Amazon_(company)."
-            #'target': a human-written explanation 
-            #'image': a PIL Image object
-            #'caption_choices': is human-written explanation
+            #'review' is an review we will use for a llama2 e.g. "Some TV programs continue into embarrassment (my beloved 'X-Files' comes to mind.) I've been a fan of Dennis Farina since 'Crime Story,' another late, lamented show. 'Buddy Faro' never had a chance. The series had a good premise and great actors. It's really, really a shame."
+            #'label': a label
+            #'evidences': a human-written important phrases
 
         res[spl_name] = cur_spl
     return res
@@ -48,20 +48,20 @@ def newyorker_caption_contest_data(args):
 
 def newyorker_caption_contest_llama2(args): 
     print("Loading data")
-    #nyc_data = newyorker_caption_contest_data(args)
-    #nyc_data_five_val = random.sample(nyc_data['test'],5)
-    #nyc_data_train_two = random.sample(nyc_data['train'],2)
+    nyc_data = newyorker_caption_contest_data(args.task_name)
+    nyc_data_five_val = random.sample(nyc_data['val'],5)
+    nyc_data_train_two = random.sample(nyc_data['train'],2)
     
-    print ("Loading data")
-    nyc_data_five_val = []
-    with jsonlines.open('out/val.jsonl') as reader:
-        for obj in reader:
-            nyc_data_five_val.append(obj)
+    #print ("Loading data")
+    #nyc_data_five_val = []
+    #with jsonlines.open('out/val.jsonl') as reader:
+    #    for obj in reader:
+    #        nyc_data_five_val.append(obj)
 
-    nyc_data_train_two = []
-    with jsonlines.open('out/train.jsonl') as reader:
-        for obj in reader:
-            nyc_data_train_two.append(obj)
+    #nyc_data_train_two = []
+    #with jsonlines.open('out/train.jsonl') as reader:
+    #    for obj in reader:
+    #        nyc_data_train_two.append(obj)
 
     from transformers import AutoModelForCausalLM, AutoTokenizer
     import torch
@@ -97,25 +97,34 @@ def newyorker_caption_contest_llama2(args):
 
     for i, val_inst in enumerate(nyc_data_five_val):         
         # ======================> ADD YOUR CODE TO DEFINE A PROMPT WITH TWO TRAIN EXAMPLES/DEMONSTRATIONS/SHOTS <======================
-        prompt =  "Please use yes or not to answer whether the movie review is positive or not and then use report important phrases to explain the reason?" + \
-            nyc_data_train_two[0]['input'] + "[/INST]" + nyc_data_train_two[0]['target'] + "</s><s>[INST]" + \
-                "Please use yes or not to answer whether the movie review is positive or not and then use report important phrases to explain the reason?" +\
-                      nyc_data_train_two[1]['input'] + "[/INST]"  + nyc_data_train_two[1]['target'] + "</s><s>[INST]" + \
-                        "According to the above two examples, please use yes or not to answer whether the movie review is positive or not and then use report important phrases to explain the reason?" + \
-                              val_inst['input'] + "[/INST]"         
+        label_train_0 = "yes" if nyc_data_train_two[0]['target']==1 else "no" # 0 = negative, 1 = positive
+        label_train_1 = "yes" if nyc_data_train_two[1]['target']==1 else "no" # 0 = negative, 1 = positive
+        #prompt =  "Please use yes or not to answer whether the movie review is positive or not and then use report important phrases to explain the reason?" +\
+        #    nyc_data_train_two[0]['input'] + "[/INST]" + label_train_0 +\
+        #        " the important phrases are " + str(nyc_data_train_two[0]['highlight']) + "</s><s>[INST]" +\
+        #            "Please use yes or not to answer whether the movie review is positive or not and then use report important phrases to explain the reason?" +\
+        #              nyc_data_train_two[1]['input'] + "[/INST]"  + label_train_1 +\
+        #                 " the important phrases are " + str(nyc_data_train_two[1]['highlight']) + "</s><s>[INST]" +\
+        #                    "According to the above two examples, please use yes or not to answer whether the movie review is positive or not and then use report important phrases to explain the reason?" +\
+        #                      val_inst['input'] + "[/INST]"     
+        prompt =  "Please use yes or not to answer whether the movie review is positive or not and then use report important phrases to explain the reason?" +\
+            nyc_data_train_two[0]['input'] + "[/INST]" + label_train_0 +\
+                "According to the above example, please use yes or not to answer whether the movie review is positive or not and then use report important phrases to explain the reason?" +\
+                    val_inst['input'] + "[/INST]"    
 
         sequences = pipeline(
             prompt,
             do_sample=True,
             eos_token_id=tokenizer.eos_token_id,
-            max_length=1024,
+            max_length=4096,
         )
         
         gen_expl = sequences[0]['generated_text'].split("/INST]")[-1]
         nyc_data_five_val[i]['generated_llama2']=gen_expl
 
         #calculate Intersection-over-Union (IOU) F1
-        target_explanation = nyc_data_five_val[i]['target']
+        label_val = "yes" if nyc_data_five_val[i]['target']==1 else "no" # 0 = negative, 1 = positive
+        target_explanation = label_val + " the important phrases are " + str(nyc_data_five_val[i]['highlight'])
         iou_f1 = iou_f1_score(gen_expl, target_explanation)
         nyc_data_five_val[i]['iou_f1_score'] = iou_f1
 
@@ -136,7 +145,7 @@ if __name__ == '__main__':
     parser.add_argument('--seed', default = 1418772, type=int, help='Random seed set to your uNID') # <======================> 
     parser.add_argument('--output_dir', type=str, help='Directory where model checkpoints will be saved')
     parser.add_argument('--cache_dir', type=str, help='Directory where cache will be saved')
-    parser.add_argument('--task_name', default="imdb",  type=str, help='Name of the task that will be used by huggingface load dataset')    
+    parser.add_argument('--task_name', default="movie_rationales",  type=str, help='Name of the task that will be used by huggingface load dataset')    
     #parser.add_argument('--subtask', default="explanation", type=str, help="The contest has three subtasks: matching, ranking, explanation")
     parser.add_argument('--llama2_checkpoint', default="meta-llama/Llama-2-7b-chat-hf", type=str, help="The hf name of a llama2 checkpoint")
     args = parser.parse_args()
